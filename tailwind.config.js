aa404c4cb8e8cb5d9f14fc1095b83665df4a0dc1
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2024-07-31 21:42:20 +0100

    feat: :lipstick: Add color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				background: '#170F11',
+				accent: '#F47D9D',
+				primary: '#E2EAFD',
+				secondary: '#CDCBCD',
+				card: {
+					stroke: '#2D2029',
+					background: '#1E151B',
+					primary: '#E2EAFD',
+					secondary: '#C6ABBC'
+				}
+			},
+			fontFamily: {
+				sans: ['Open Sans', 'Poppins']
+			}
+		}
 	},
 	plugins: []
 };
