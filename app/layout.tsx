b8490e948018d12c48d793cf41f05bc213633bde
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2024-11-18 13:14:32 +0000

    feat: :sparkles: added @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -30,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
